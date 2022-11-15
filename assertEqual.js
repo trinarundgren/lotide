8000484130597c22a8ba917194a1658e038b8ff2
Trina <trina.rundgren@telus.com>
2022-11-14 19:26:36 -0800

    assert assignment

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🌻 Assertion passed: ${actual} === ${expected}');
+  
+  } else {
+    console.log('🤬 Assertion failed: ${actual} !== ${expected}');
+  }
+
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
