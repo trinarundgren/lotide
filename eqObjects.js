9efbae10aafa16829b2ed956ff360f6a365bd651
Trina <trina.rundgren@telus.com>
2022-11-17 20:27:14 -0800

    final

---
+++ eqObjects.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌻🌻🌻Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☔☔☔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = require("./eqArrays");
+
+const eqObjects = function(object1, object2) {
+  let output = true;
+  const array1 = Object.keys(object1);
+  const array2 = Object.keys(object2);
+  if (array1.length !== array2.length) output = output && false;
+  else for (let key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      output = output && (eqArrays(object1[key], object2[key]));
+    } else if (typeof(object1[key]) === "object" || typeof(object2[key]) === "object") {
+      output = output && eqObjects(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) output = output && false;
+  }
+  return output;
+};
+
---
+++ middle.js
@@ -0,0 +1,16 @@
+const middle = function(arr) {
+  const tooShortForMiddle = arr.length <= 2;
+  const evenArray = arr.length % 2 === 0;
+  if (tooShortForMiddle) {
+    return [];
+  } else if (evenArray) {
+    const firstMiddleIndex = (arr.length / 2) - 1;
+    const secondMiddleIndex = (arr.length / 2);
+    return [arr[firstMiddleIndex], arr[secondMiddleIndex]];
+  } else {
+    const singleMiddleIndex = (Math.floor(arr.length / 2));
+    return [arr[singleMiddleIndex]];
+  }
+};
+
+module.exports = middle;
