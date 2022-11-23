f781cb2b6b0a14578a0a41ea82947fca48ff99dc
Trina <trina.rundgren@telus.com>
2022-11-23 14:27:33 -0800

    final version

--- without.js
+++ without.js
@@ -1,20 +1,25 @@
-// const eqArrays = function(array1, array2) {
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   } return true;
-// };
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`🌻🌻🌻Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`☔☔☔Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
 
-const without = function(source, itemsToRemove) {
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌻🌻🌻Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☔☔☔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function (source, itemsToRemove) {
   for (let i = 0; i < source.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
       if (source[i] === itemsToRemove[j]) {
@@ -26,12 +31,12 @@ const without = function(source, itemsToRemove) {
   return source;
 };
 
-assertArraysEqual(without([1,2,3], [1]), [2,3]);
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+// assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 
-const words = ["hello", "world", "lighthouse"];
-without(["hello", "world", "lighthouse"], ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// const words = ["hello", "world", "lighthouse"];
+// without(["hello", "world", "lighthouse"], ["lighthouse"]);
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 
 module.exports = without;
