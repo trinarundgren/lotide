36010f7daf26490ddcf14d43b12d13efed79b4f1
Trina <trina.rundgren@telus.com>
2022-11-23 14:52:56 -0800

    final version

--- countOnly.js
+++ countOnly.js
@@ -1,14 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🌻🌻🌻Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`☔☔☔Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// ASSERT EQUAL...
+const assertEqual = require('./assertEqual');
 
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+// COUNT-ONLY FUNCTION
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
