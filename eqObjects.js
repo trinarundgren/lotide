54fbecefafe0ae7ea7dc20e630717bfe8f790cb9
Trina <trina.rundgren@telus.com>
2022-11-23 15:35:35 -0800

    final version

--- eqObjects.js
+++ eqObjects.js
@@ -24,3 +24,7 @@ const eqObjects = function(object1, object2) {
   return output;
 };
 
+// // TEST CODE
+// console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
