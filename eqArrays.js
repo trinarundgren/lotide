cc43c7e109b2f085b3bcfa686a695bbd12c1cd86
Trina <trina.rundgren@telus.com>
2022-11-23 14:10:56 -0800

    final version

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,23 @@
-const eqArrays = function(array1, array2) {
-  
+// ASSERT EQUAL
+const assertEqual = require('./assertEqual').default;
+
+// EQUAL ARRAYS FUNCTION
+const eqArrays = function (array1, array2) {
+
   if (array1.length !== array2.length) {
     // console.log("Arrays not equal length")
     return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+        // if it is an array && the arrays are not equal then return false;
+        return false;
+      } else if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+        // if the element is not an array and the items are not equal then return false
+        return false;
+      }
+    }
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-      // if it is an array && the arrays are not equal then return false;
-      return false;
-    }
-    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-      // if the element is not an array and the items are not equal then return false
-      return false;
-    }
-  } 
   return true;
 };
 
