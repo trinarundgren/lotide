8245ae30817830f31b75f93350d7ccb715babaa2
Trina <trina.rundgren@telus.com>
2022-11-23 14:43:29 -0800

    final version

--- middle.js
+++ middle.js
@@ -1,4 +1,11 @@
-const middle = function(arr) {
+// EQUAL ARRAYS...
+const eqArrays = require('./eqArrays');
+
+// ASSERT ARRAYS EQUAL...
+const assertArraysEqual = require('./assertArraysEqual');
+
+// MIDDLE FUNCTION
+const middle = function (arr) {
   const tooShortForMiddle = arr.length <= 2;
   const evenArray = arr.length % 2 === 0;
   if (tooShortForMiddle) {
