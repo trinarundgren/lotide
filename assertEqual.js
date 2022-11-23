221bbc803eddbe15d277b967b7eadc91f2f671b5
Trina <trina.rundgren@telus.com>
2022-11-23 13:45:13 -0800

    final version

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-// module.exports = assertEqual;
+module.exports = assertEqual;
+// TEST CODE - ignore this
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
