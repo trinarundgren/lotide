de49650d4b575ff161cdae1ff19db665bf0e97ec
Trina <trina.rundgren@telus.com>
2022-11-23 15:45:36 -0800

    final version

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,9 +1,9 @@
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // <= add this line
   if (eqObjects(actual, expected)) {
-    console.log(`?????? Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`🌻🌻🌻 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`??? Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    console.log(`☔☔☔ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
