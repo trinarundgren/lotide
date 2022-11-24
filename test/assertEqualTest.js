0e7985a7ee4016d547038940b09a959ce5525df4
Trina <trina.rundgren@telus.com>
2022-11-23 16:14:35 -0800

    final version

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+// ASSERT EQUAL...
+const assertEqual = require('../assertEqual');
+
+
+// TEST CODE
+assertEqual('Bootcamp', 'Bootcamp');
+assertEqual(1,1);
+assertEqual('MommySaurusRex', 'MommySaurusRex');
